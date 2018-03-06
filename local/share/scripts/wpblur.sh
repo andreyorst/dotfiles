0fe043208c62db2874c55f4655c1bba05cbae440
Andrey Orst <andreyorst@gmail.com>
2018-03-06 21:54:14 +0300

    bluuuuuuuuuur

--- local/share/scripts/wpblur.sh
+++ local/share/scripts/wpblur.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+sleep 10
 while true; do
-    inotifywait -mq ~/.config/plasma-org.kde.plasma.desktop-appletsrc -e access -e modify -e delete || sleep 1 | while read; do
-         convert $(cat ~/.config/plasma-org.kde.plasma.desktop-appletsrc | grep "Image=file" | sed 's/.*\/\//\//') -filter Gaussian -resize 5% -define filter:sigma=2.5 -resize 2000% -attenuate 0.3 +noise Gaussian ~/.bg.png
-         cp ~/.bg.png /usr/share/sddm/themes/breeze/components/artwork/background.png
+    inotifywait -q ~/.config/plasma-org.kde.plasma.desktop-appletsrc -e delete_self | while read; do
+        sleep 1
+        convert $(cat ~/.config/plasma-org.kde.plasma.desktop-appletsrc | grep "Image=file" | sed 's/.*\/\//\//') -filter Gaussian -resize 5% -define filter:sigma=2.5 -resize 2000% -attenuate 0.2 +noise Gaussian ~/.bg.png
+        cp ~/.bg.png /usr/share/sddm/themes/breeze/.bg.png
     done
 done
