37455241d2a3682e555622638dd57113f9c4dc74
Andrey Orst <andreyorst@gmail.com>
2018-03-07 08:48:17 +0300

    update

---
+++ local/share/konsole/Default.profile
@@ -0,0 +1,12 @@
+[Appearance]
+ColorScheme=Tomorrow Night
+LineSpacing=0
+
+[General]
+Name=Default
+Parent=FALLBACK/
+ShowTerminalSizeHint=false
+
+[Scrolling]
+HistoryMode=2
+ScrollBarPosition=2
--- local/share/scripts/wpblur.sh
+++ local/share/scripts/wpblur.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sleep 10
 while true; do
     inotifywait -q ~/.config/plasma-org.kde.plasma.desktop-appletsrc -e delete_self | while read; do
-        sleep 1
+        sleep 2
         convert $(cat ~/.config/plasma-org.kde.plasma.desktop-appletsrc | grep "Image=file" | sed 's/.*\/\//\//') -filter Gaussian -resize 5% -define filter:sigma=2.5 -resize 2000% -attenuate 0.2 +noise Gaussian ~/.bg.png
         cp ~/.bg.png /usr/share/sddm/themes/breeze/.bg.png
     done
