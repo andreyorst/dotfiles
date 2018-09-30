db6f50b300bf9a8309aee7f1c863099aa4e7dbf8
Andrey Orst <andreyorst@gmail.com>
2018-09-30 21:46:05 +0300

    update default font

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,5 +13,17 @@
 (tool-bar-mode -1)
 
 ;; default font:
-(set-face-attribute 'default nil :font "Liberation Mono-10")
-;; (set-face-attribute 'default t :font "Liberation Mono-10")
+(set-frame-font "Source Code Pro-10" nil t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (orgtbl-show-header use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
