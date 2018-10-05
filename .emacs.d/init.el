4b5d1a7ec369c53f7cda76a3721291daedd73b5a
andreyorst <andreyorst@gmail.com>
2018-10-05 11:16:59 +0300

    fix custom

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,11 +5,12 @@
   (tool-bar-mode -1)
   (tooltip-mode -1)
   (fset 'menu-bar-open nil)
-  (set-face-attribute 'default nil :font "Source Code Pro-10")
-  (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-  (load custom-file :noerror))
+  (set-face-attribute 'default nil :font "Source Code Pro-10"))
 
-(defmacro ensure-installed (pkg)
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file :noerror)
+
+(defun ensure-installed (pkg)
   "Ensure that plugin is installed while loading Emacs"
   (when (not (package-installed-p pkg))
     (package-install pkg)))
@@ -22,13 +23,13 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(ensure-installed use-package)
+(ensure-installed 'use-package)
 (require 'use-package)
 
-(ensure-installed spacemacs-theme)
+(ensure-installed 'spacemacs-theme)
 (load-theme 'spacemacs-dark t nil)
 
-(ensure-installed highlight-indent-guides)
+(ensure-installed 'highlight-indent-guides)
 (use-package highlight-indent-guides
   :init
   (setq highlight-indent-guides-method 'character
@@ -37,25 +38,25 @@
   :config
   (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
 
-(ensure-installed helm)
+(ensure-installed 'helm)
 (use-package helm
   :bind (("M-x" . helm-M-x)
          ("C-x C-f" . helm-find-files)
          ("C-x C-b" . helm-buffers-list)
          ("C-x M-f" . helm-recentf)))
 
-(ensure-installed spaceline)
+(ensure-installed 'spaceline)
 (use-package spaceline-config
   :config
   (spaceline-spacemacs-theme)
   (spaceline-helm-mode))
 
-(ensure-installed iedit)
+(ensure-installed 'iedit)
 (use-package iedit
   :bind
   ("C-;" . iedit-mode))
 
-(ensure-installed markdown-mode)
+(ensure-installed 'markdown-mode)
 (use-package markdown-mode)
 
 ;; C specific settings
@@ -64,9 +65,9 @@
           (setq c-basic-offset 4)
           (setq-default tab-width 4))
 
-(ensure-installed yasnippet-snippets)
+(ensure-installed 'yasnippet-snippets)
 (use-package yasnippet-snippets)
-(ensure-installed yasnippet)
+(ensure-installed 'yasnippet)
 (use-package yasnippet
   :config
   (add-hook 'prog-mode-hook 'yas-minor-mode))
