f796f3768a860854bb0042eb9adac6a336ba3392
Andrey Orst <andreyorst@gmail.com>
2018-10-07 22:23:15 +0300

    replace helm with ivy

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,3 @@
-;; Common settings
 (ignore-errors
   (menu-bar-mode -1)
   (scroll-bar-mode -1)
@@ -36,37 +35,52 @@
 (ensure-installed 'spacemacs-theme)
 (load-theme 'spacemacs-dark t nil)
 
-(use-package helm :ensure t
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x C-b" . helm-buffers-list)
-         ("C-x M-f" . helm-recentf)))
+(use-package ivy :ensure t
+  :ensure swiper
+  :ensure counsel
+  :init
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  :bind (("C-s" . swiper)
+         ("C-c C-r" . ivy-resume)
+         ("<f6>" . ivy-resume)
+         ("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("<f1> f" . counsel-describe-function)
+         ("<f1> v" . counsel-describe-variable)
+         ("<f1> l" . counsel-find-library)
+         ("<f2> i" . counsel-info-lookup-symbol)
+         ("<f2> u" . counsel-unicode-char)
+         ("C-c g" . counsel-git)
+         ("C-c j" . counsel-git-grep)
+         ("C-c k" . counsel-ag)
+         ("C-x l" . counsel-locate)
+         ("C-S-o" . counsel-rhythmbox))
+  :config
+  (ivy-mode 1)
+  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
 
 (use-package spaceline-config :ensure spaceline
   :config
   (spaceline-emacs-theme)
-  (spaceline-helm-mode)
+  ;; (spaceline-helm-mode)
   :init
   (setq powerline-default-separator 'slant))
 
-(use-package iedit :ensure t
-  :bind
-  ("C-;" . iedit-mode))
+(use-package iedit :ensure t)
 
 (use-package markdown-mode
   :ensure markdown-mode)
 
-(use-package multiple-cursors
+(use-package multiple-cursors :ensure t
   :bind
-  ("C-c >" . mc/mark-next-like-this)
-  ("C-c <" . mc/mark-previous-like-this)
-  ("C-c w" . mc/mark-next-word-like-this)
-  ("C-c M-w" . mc/mark-previous-word-like-this))
+  ("C-d" . mc/mark-next-like-this))
+
+(use-package lsp-mode :ensure t)
 
 (use-package company :ensure t
   :init
   (setq company-require-match 'never
-        company-transformers 'nil
         company-minimum-prefix-length 2
         company-lsp-async t
         company-lsp-cache-candidates nil
@@ -77,25 +91,28 @@
   :config
   (setq company-backends (remove 'company-clang company-backends)
         company-backends (remove 'company-xcode company-backends)
-        company-backends (remove 'company-gtags company-backends)
         company-backends (remove 'company-cmake company-backends))
+        company-backends (remove 'company-gtags company-backends)
+        company-transformers 'company-sort-prefer-same-case-prefix
   (add-hook 'after-init-hook 'global-company-mode)
   (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
   (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
   (define-key company-active-map (kbd "S-TAB") 'company-select-previous)
   (define-key company-active-map (kbd "<backtab>") 'company-select-previous))
 
+(use-package company-lsp :ensure t
+  :config
+  (push '(company-lsp :with company-yasnippet) company-backends))
+
+(use-package company-quickhelp :ensure t
+  :config
+  (company-quickhelp-mode))
+
 (use-package yasnippet :ensure t
   :config
   (add-hook 'prog-mode-hook 'yas-minor-mode))
 
 (use-package yasnippet-snippets :ensure t)
-
-(use-package company-lsp :ensure t
-  :config
-  (push '(company-lsp :with company-yasnippet) company-backends))
-
-(use-package lsp-mode :ensure t)
 
 (use-package nlinum :ensure t
   :config
