8e9b0d483987fcbf65815a036ca286f6465703a4
Andrey Orst <andreyorst@gmail.com>
2018-10-10 19:52:25 +0300

    update

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,7 +6,7 @@
  '(company-transformers (quote (company-sort-prefer-same-case-prefix)))
  '(package-selected-packages
    (quote
-    (sr-speedbar lsp-ui flycheck projectile highlight-indent-guides cquery nlinum yasnippet-snippets yasnippet company-quickhelp company-lsp company lsp-mode multiple-cursors markdown-mode iedit spaceline counsel swiper ivy spacemacs-theme use-package))))
+    (company-flx flx diminish sr-speedbar lsp-ui flycheck projectile highlight-indent-guides cquery nlinum yasnippet-snippets yasnippet company-quickhelp company-lsp company lsp-mode multiple-cursors markdown-mode iedit spaceline counsel swiper ivy spacemacs-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
