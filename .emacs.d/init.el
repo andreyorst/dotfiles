08223c55acb6f2bb17f73b0648238ba3f8f0477e
Andrey Orst <andreyorst@gmail.com>
2018-11-28 16:35:15 +0300

    add parinfer

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,7 +19,7 @@
   (tool-bar-mode -1)
   (tooltip-mode -1)
   (fset 'menu-bar-open nil))
-(set-face-attribute 'default nil :font "Source Code Pro-11")
+(set-face-attribute 'default nil :font "Source Code Pro-10")
 
 (setq-default indent-tabs-mode nil
               scroll-step 1
@@ -43,6 +43,24 @@
 (ensure-installed 'spacemacs-theme)
 (load-theme 'spacemacs-dark t nil)
 (set-face-attribute 'fringe nil :background nil)
+
+(use-package parinfer
+  :ensure t
+  :bind
+  (("C-," . parinfer-toggle-mode))
+  :init
+  (progn
+    (setq parinfer-extensions
+          '(defaults       ;; should be included.
+            pretty-parens  ;; different paren styles for different modes.
+            paredit        ;; Introduce some paredit commands.
+            smart-tab      ;; C-b & C-f jump positions and smart shift with tab & S-tab.
+            smart-yank))   ;; Yank behavior depend on mode.
+    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'scheme-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode)))
 
 (use-package diminish :ensure t)
 
