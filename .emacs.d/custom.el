1a30c717762fed74a1252ff03bf70d13b3d224ec
Andrey Orst <andreyorst@gmail.com>
2018-11-29 18:32:06 +0300

    add custom because I'm dumb and don't know how to set faces without it

---
+++ .emacs.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet-snippets yasnippet company-quickhelp company flycheck markdown-mode counsel ivy flx highlight-indent-guides parinfer diminish telephone-line spacemacs-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(line-number ((t (:inherit default :background "#292b2e" :foreground "gray34"))))
+ '(line-number-current-line ((t (:inherit line-number :background "#292b2e" :foreground "#bc6ec5" :weight bold)))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -72,11 +72,10 @@
   :init
   (progn
     (setq parinfer-extensions
-          '(defaults       ;; should be included.
-            pretty-parens  ;; different paren styles for different modes.
-            paredit        ;; Introduce some paredit commands.
-            smart-tab      ;; C-b & C-f jump positions and smart shift with tab & S-tab.
-            smart-yank))   ;; Yank behavior depend on mode.
+          '(defaults        ;; should be included.
+             pretty-parens  ;; different paren styles for different modes.
+             smart-tab      ;; C-b & C-f jump positions and smart shift with tab & S-tab.
+             smart-yank))   ;; Yank behavior depend on mode.
     (add-hook 'clojure-mode-hook #'parinfer-mode)
     (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
     (add-hook 'common-lisp-mode-hook #'parinfer-mode)
