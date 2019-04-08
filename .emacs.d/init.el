f9ec869cb16093d65e374299e7a67689559a4cdf
Andrey Orst <andreyorst@gmail.com>
2019-04-08 20:08:37 +0300

    faster startup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,8 +3,41 @@
 ;;; Emacs config by Andrey Orst
 ;;; Code:
 
+;; speed hacks from Doom Emacs
+
+;; Avoid garbage collection during startup. The GC eats up quite a bit of
+;; time, easily doubling startup time. The trick is to turn up the memory
+;; threshold to prevent it from running
+
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+;; unset file-name-handler-alist
+
+(defvar my--file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+;; Package.el initialization is expensive, so disable it too
+
+;; (setq package-enable-at-startup nil ; don't auto-initialize!
+;;       ;; don't add that `custom-set-variables' block to my initl!
+;;       package--init-file-ensured t)
+
+;; Configuration with org-babel
+
 (require 'org)
 (org-babel-load-file "~/.emacs.d/config.org")
+
+;; reset garbace collection
+
+(add-hook 'emacs-startup-hook
+          (setq gc-cons-threshold 16777216
+                gc-cons-percentage 0.1))
+
+;; restore file-name-handler-alist
+
+(add-hook 'emacs-startup-hook
+          (setq file-name-handler-alist my--file-name-handler-alist))
 
 (provide 'init)
 ;;; init.el ends here
