f30c2f53c09af4dd0df6f88cf11bee64f24f6982
Andrey Orst <andreyorst@gmail.com>
2019-04-22 22:13:38 +0300

    actually make center view a buffer local mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -264,6 +264,12 @@ are defining or executing a macro."
 
 (defvar center-view-mode-hook nil)
 (defvar center-view-mode nil)
+(defvar center-view-extra-space 10)
+
+(make-variable-buffer-local
+ (progn
+   (defvar center-view--margin-size 0 "the size of margins in the buffer")
+   (defvar center-view--buffer-window nil)))
 
 ;;;###autoload
 (define-minor-mode center-view-mode
@@ -273,6 +279,7 @@ This mode makes buffer contents centered."
   :lighter " center-view"
   (if center-view-mode
       (progn
+        (setq center-view--buffer-window (get-buffer-window))
         (add-hook 'window-size-change-functions
                   'center-view)
         (center-view))
@@ -281,12 +288,18 @@ This mode makes buffer contents centered."
                    'center-view)
       (set-window-margins nil 0 0))))
 
-(make-variable-buffer-local
- (defvar margin-size 0 "the size of margins in the buffer"))
-
-(defun center-view (&optional window)
-  (let ((margin-size (/ (- (frame-width) (+ fill-column 10)) 2)))
-    (set-window-margins (frame-root-window window) margin-size margin-size)))
+(defun center-view (&optional frame)
+  (if (>= (window-total-width)
+          (+ fill-column center-view-extra-space))
+      (if center-view--buffer-window
+          (let ((center-view--margin-size
+                 (/ (- (window-total-width center-view--buffer-window)
+                       (+ fill-column
+                          center-view-extra-space))
+                    2)))
+            (set-window-margins center-view--buffer-window
+                                center-view--margin-size
+                                center-view--margin-size)))))
 
 (provide 'center-view-mode)
 
