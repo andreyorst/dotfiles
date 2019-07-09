f79fba9fc84bdc2f3c2f834807d477cb0cfcb40f
Andrey Orst <andreyorst@gmail.com>
2019-07-09 12:24:43 +0300

    remove heading height settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -544,11 +544,7 @@ are defining or executing a macro."
                   org-highlight-latex-and-related '(latex)
                   revert-without-query '(".*\.pdf")
                   org-preview-latex-default-process 'dvisvgm)
-            (auto-fill-mode)
-            (set-face-attribute 'org-document-title nil :height 1.6)
-            (set-face-attribute 'org-level-1        nil :height 1.4)
-            (set-face-attribute 'org-level-2        nil :height 1.2)
-            (set-face-attribute 'org-level-3        nil :height 1.0)))
+            (auto-fill-mode)))
 
 (setq org-src-fontify-natively t)
 
