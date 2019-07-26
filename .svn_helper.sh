dc32726431b0449c1ca24acb635757a363a4f399
Andrey Orst <andreyorst@gmail.com>
2019-07-26 11:13:44 +0300

    svn helper script

---
+++ .svn_helper.sh
@@ -0,0 +1,36 @@
+# Smarter svn command. It will use more robust versions
+# instead of builtin ones or fallback to default if there's
+# no handler for particular svn command.
+svn() {
+    cmd="$1"
+    shift
+    case "$cmd" in
+        (diff)  svn_diff "$@"           ;;
+        (log)   svn_log "$@"            ;;
+        (st)    svn_st "$@"             ;;
+        (blame) svn_blame "$@"          ;;
+        (*)     command svn "$cmd" "$@" ;;
+    esac
+}
+
+## calls svn diff with colors piped to less if needed.
+svn_diff() {
+    command svn diff -x -w "$@" | colordiff | less -RF
+}
+
+## calls svn log piped to less when log
+## takes more screen space than currently available.
+svn_log() {
+    command svn log "$@" | less -F
+}
+
+## calls svn st, but dismiss all untracked files
+svn_st() {
+    command svn st "$@" | grep -v '^?'
+}
+
+## calls svn blame, and puts it ouptup to less if needed
+svn_blame() {
+    command svn blame "$@" | less -F
+}
+
