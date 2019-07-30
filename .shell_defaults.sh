804d62bef7e3cb67951bec219be330b2edbabf87
Andrey Orst <andreyorst@gmail.com>
2019-07-30 19:53:40 +0300

    add comments

--- .shell_defaults.sh
+++ .shell_defaults.sh
@@ -1,9 +1,17 @@
+# Aliases and Defaults
+# ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+
+if [ -n "$(command -v dash)" ] && [ -n "$(command -v kak)" ]; then
+    export KAKOUNE_POSIX_SHELL=$(command -v dash)
+fi
+
 if [ -n "$(command -v exa)" ]; then
     unset LS_COLORS
     unset LSCOLORS
     alias ls="exa"
     alias la="exa -la"
     alias l="exa -l"
+    alias ll="exa -l"
 fi
 
 if [ -n "$(command -v kak)" ]; then
@@ -14,27 +22,79 @@ else
     export EDITOR="vi"
 fi
 
+alias e="$EDITOR"
+
+alias gti="git"
+alias g="git"
+
+# Edit things in apropriate environment
+alias zshconf="$EDITOR ~/.zshrc"
+alias tmuxconf="$EDITOR ~/.tmux.conf"
+alias kakconf="kak ~/.config/kak/kakrc"
+alias emacsconf="emacs ~/.emacs.d/config.org"
+
+alias less="less --tabs 4"
+
+# ultimate alias to create and attach to tmux sessions
+alias tmux="tmux new-session -d -s \>_ 2>/dev/null; tmux new-session -t \>_ \; set-option destroy-unattached"
+
+# to use sudo with aliases
+alias sudo="sudo "
+
+# Functions
+# ‾‾‾‾‾‾‾‾‾
+
+# create dir and cd into it
 mkcd() { mkdir -p $1 && cd $1; }
 
+# convert webm to mp4
 webmp4() {
+    if [ -n "$(command -v ffmpeg)" ]; then
+        echo "ffmpeg is not installed" >&2
+        return 1
+    fi
     while [ $# -ne 0 ]; do
         ffmpeg -i "$1" "${1%.*}.mp4"
         shift
     done
 }
 
-alias zshconf="$EDITOR ~/.zshrc"
-alias tmuxconf="$EDITOR ~/.tmux.conf"
-alias kakconf="kak ~/.config/kak/kakrc"
-alias emacsconf="emacs ~/.emacs.d/config.org"
-alias less="less --tabs 4"
-alias tmux="tmux new-session -d -s \>_ 2>/dev/null; tmux new-session -t \>_ \; set-option destroy-unattached"
-alias gti="git"
-alias sudo="sudo "
+# gifify wrapper for fast gif creating
+gif() {
+    if [ -n "$(command -v gifify)" ]; then
+        gifify $1 -o ${1#.*}.gif --colors 256 --compress 0 --fps 30
+    else
+        echo "gifify is not installed" >&2
+        return 1
+    fi
+}
 
-alias g="git"
-alias e="$EDITOR"
+# Create new C project from template
+cnew() {
+    if [ $# -ne 1 ]; then
+        echoerr "wrong argument count"
+        return 1
+    fi
+    mkdir $1 >/dev/null 2>&1
+    res=$?
+    if [ $res -ne 0 ]; then
+        echo "Error creating a project: $res" >&2
+        return $res
+    fi
+    cp ~/.dotfiles/.c_project_template/* $1/ >/dev/null 2>&1
+    res=$?
+    if [ $res -eq 0 ]; then
+        echo "# $1" > $1/README.md
+        echo "Created project: $1"
+    else
+        rm -rf $1
+        echo "Error creating a project: $res" >&2
+        return $res
+    fi
+}
 
+# Package manager wrappers
+# ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 for pm in yay pacman dnf yum apt; do
     if [ -n "$(command -v $pm)" ]; then
         alias pm="pm_$pm"
@@ -115,30 +175,3 @@ else
         esac
     }
 fi
-
-if [ -n "$(command -v dash)" ]; then
-    export KAKOUNE_POSIX_SHELL=$(command -v dash)
-fi
-
-cnew() {
-    if [ $# -ne 1 ]; then
-        echoerr "wrong argument count"
-        exit 1
-    fi
-    mkdir $1 >/dev/null 2>&1
-    res=$?
-    if [ $res -ne 0 ]; then
-        echo "Error creating a project: $res" >&2
-        return $res
-    fi
-    cp ~/.dotfiles/.c_project_template/* $1/ >/dev/null 2>&1
-    res=$?
-    if [ $res -eq 0 ]; then
-        echo "# $1" > $1/README.md
-        echo "Created project: $1"
-    else
-        rm -rf $1
-        echo "Error creating a project: $res" >&2
-        return $res
-    fi
-}
