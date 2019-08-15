6fc4e47044d37cac3072190096f7e43e36b8858a
Andrey Orst <andreyorst@gmail.com>
2019-08-15 15:01:28 +0300

    remove duplicate commentary

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -17,10 +17,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
 
 (provide 'custom)
 ;;; custom.el ends here
