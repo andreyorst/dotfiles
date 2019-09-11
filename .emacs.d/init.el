03ca5781c845c4c2b3896a53cab4d48295d8c2e8
Andrey Orst <andreyorst@gmail.com>
2019-09-11 09:06:56 +0300

    remove smart beginning of line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -94,18 +94,6 @@ are defining or executing a macro."
 
 (setq command-error-function #'my/command-error-function)
 
-(defun my/smart-move-beginning-of-line ()
-  "Move point to first non-whitespace character or beginning-of-line.
-
-Move point to the first non-whitespace character on this line.
-If point was already at that position, move point to beginning of line."
-  (interactive)
-  (let ((oldpos (point)))
-    (back-to-indentation)
-    (when (= oldpos (point))
-      (beginning-of-line))))
-(global-set-key [remap move-beginning-of-line] #'my/smart-move-beginning-of-line)
-
 (setq inhibit-splash-screen t
       initial-major-mode 'org-mode
       initial-scratch-message "")
