ee8af743320ae64e6840a792759de3fb23a305ec
Andrey Orst <andreyorst@gmail.com>
2020-07-28 20:54:11 +0300

    restructure shell stuff

--- .bashrc
+++ .bashrc
@@ -10,39 +10,16 @@ for i in $HOME/.dotfiles/scripts/*.sh; do
     . "$i"
 done
 
-
-if [ -z "${PATH##*termux*}" ]; then
-    # Termux doesn't require sudo
-    alias sudo=""
-else
-    # Hack to make sudo work with aliases
-    alias sudo="sudo "
-fi
-
-# place to install npm packages
-NPM_PACKAGES="${HOME}/.npm-packages"
-# Preserve MANPATH if you already defined it somewhere in your config.
-# Otherwise, fall back to `manpath` so we can inherit from `/etc/manpath`.
-export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
-
 # User specific environment
-if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
-    PATH="$HOME/.dotfiles/scripts:$HOME/.local/bin:$HOME/bin:$NPM_PACKAGES/bin:$PATH"
-fi
-export PATH
+export PATH="$HOME/.dotfiles/scripts:$HOME/.local/bin:$PATH"
 
-unset LS_COLORS
-
-# classyTouch Prompt
+# Prompt
 PS1="\[\e[0;31m\]┌─╼[\[\e[m\]\w\[\e[0;31m\]] \$SSH_PS1\$CONTAINER_PS1\$GIT_PS1
 \$(if [ \$? -eq 0 ]; then echo \"\[\e[0;31m\]└────╼\"; else echo \"\[\e[0;31m\]└╼\"; fi) \[\e[m\]"
 
-# Avoid duplicates
-HISTCONTROL=ignoredups:erasedups
-# When the shell exits, append to the history file instead of overwriting it
-shopt -s histappend
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -n; git_ps1; ssh_ps1; screen_ps1; container_ps1"
 
-git_ps1() {
+function git_ps1() {
     if git rev-parse --is-inside-work-tree 1>/dev/null 2>&1; then
         branch=$(git symbolic-ref --short HEAD 2>/dev/null)
         [ $? -ne 0 ] && branch=$(git rev-parse --short HEAD)
@@ -56,7 +33,7 @@ git_ps1() {
     fi
 }
 
-ssh_ps1() {
+function ssh_ps1() {
     if [ -n "$SSH_CONNECTION" ]; then
         SSH_PS1="[$(tput sgr0)ssh$(tput setaf 1)] "
     else
@@ -64,14 +41,14 @@ ssh_ps1() {
     fi
 }
 
-screen_ps1() {
+function screen_ps1() {
     case "$TERM" in
         screen*) SCREEN_PS1="[$(tput sgr0)screen$(tput setaf 1)] " ;;
         *) SCREEN_PS1= ;;
     esac
 }
 
-container_ps1() {
+function container_ps1() {
     if [ -e /run/.toolboxenv ]; then
         CONTAINER_PS1="[$(tput sgr0)toolbox$(tput setaf 1)] "
     elif [ -e /run/.containerenv  ]; then
@@ -83,9 +60,8 @@ container_ps1() {
     fi
 }
 
-# After each command, append to the history file and reread it
-PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r; git_ps1; ssh_ps1; screen_ps1; container_ps1"
 
+# Tmux
 if [ -n "$(command -v tmux)" ] && [ -z "$TMUX" ]; then
     tmux
 fi
--- scripts/shell_defaults.sh
+++ scripts/shell_defaults.sh
@@ -1,59 +1,56 @@
 # Aliases and Defaults
 # ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 
-if [ -n "$(command -v dash)" ] && [ -n "$(command -v kak)" ]; then
-    export KAKOUNE_POSIX_SHELL=$(command -v dash)
-fi
+unset LS_COLORS
+unset LSCOLORS
 
-if [ -n "$(command -v exa)" ]; then
-    unset LS_COLORS
-    unset LSCOLORS
-    alias ls="exa"
-    alias la="exa -la"
-    alias l="exa -l"
-    alias ll="exa -l"
-fi
-
-if [ -n "$(command -v kak)" ]; then
-    export EDITOR="kak"
-elif [ -n "$(command -v emacs)" ]; then
-    export EDITOR="emacs"
-else
-    export EDITOR="vi"
-fi
-
-alias e="$EDITOR"
+# Hack to make sudo work with aliases
+alias sudo="sudo "
 
 alias gti="git"
 alias g="git"
 
+[ -n "$(command -v tmux)" ] && alias tmux="tmux new-session -d -s \>_ 2>/dev/null; tmux new-session -t \>_ \; set-option destroy-unattached"
+
 # Edit things in apropriate environment
-alias zshconf="$EDITOR ~/.zshrc"
 alias tmuxconf="$EDITOR ~/.tmux.conf"
-alias kakconf="kak ~/.config/kak/kakrc"
 alias emacsconf="emacs ~/.emacs.d/README.org"
 
-alias less="less --tabs 4"
+alias less="less --tabs 4 -R"
 
-# ultimate alias to create and attach to tmux sessions
-if [ -n "$(command -v tmux)" ]; then
-    alias tmux="tmux new-session -d -s \>_ 2>/dev/null; tmux new-session -t \>_ \; set-option destroy-unattached"
+if [ -n "$(command -v exa)" ]; then
+    alias ls="exa"
+    alias la="exa -la"
+    alias l="exa -l"
+    alias ll="exa -l"
+else
+    alias ll="ls -l"
+    alias l="ls -l"
+    alias la="ls -la"
 fi
 
-# to use sudo with aliases
-alias sudo="sudo "
+if [ -n "$(command -v emacs)" ]; then
+    export EDITOR="emacs"
+elif [ -n "$(command -v vim)" ]; then
+    export EDITOR="vim"
+else
+    export EDITOR="vi"
+fi
 
-# Functions
-# ‾‾‾‾‾‾‾‾‾
+HISTCONTROL=ignoredups:erasedups
+HISTIGNORE='ls:ll:cd:pwd:bg:fg:history'
+HISTSIZE=100000
+HISTFILESIZE=10000000
+shopt -s histappend
 
 # smarter `emacs' launcher that will open file in existing emacs if one exists
-emacs() { emacsclient -a 'emacs' -n "$@" 2>/dev/null || command emacs; }
+function emacs() { [ $# -gt 0 ] && emacsclient -a emacs -n "$@" || command emacs; }
 
 # create dir and cd into it
-mkcd() { mkdir -p $1 && cd $1; }
+function mkcd() { mkdir -p "$1" && cd "$1"; }
 
 # convert webm to mp4
-webmp4() {
+function webmp4() {
     if [ -z "$(command -v ffmpeg)" ]; then
         printf "%s\n" "ffmpeg is not installed" >&2
         return 1
@@ -64,7 +61,7 @@ webmp4() {
     done
 }
 
-scour() {
+function scour() {
     if [ -z "$(command -v scour)"  ]; then
         printf "%s\n" "scour is not installed" >&2
         return 1
@@ -77,16 +74,15 @@ scour() {
 }
 
 # gifify wrapper for fast gif creating
-gif() {
-    if [ -n "$(command -v gifify)" ]; then
-        gifify $1 -o ${1#.*}.gif --colors 256 --compress 0 --fps 30
-    else
+function gif() {
+    if [ -z "$(command -v gifify)" ]; then
         printf "%s\n" "gifify is not installed" >&2
         return 1
     fi
+    gifify $1 -o ${1#.*}.gif --colors 256 --compress 0 --fps 30
 }
 
-sep() {
+function sep() {
     for i in $(seq 1 $(tput cols)); do
         sep="${sep}="
     done
