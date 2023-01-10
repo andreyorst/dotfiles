6ab933340f18a42b8624321fdc77f8831d4378ac
Andrey Listopadov <a.listopadov@rt-solar.ru>
2023-01-10 19:08:16 +0300

    add lsp server for scala

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1952,6 +1952,15 @@ returned is test, otherwise it's src."
   :no-require t
   :hook (java-mode . lsp))
 
+(use-package lsp-metals
+  :straight t
+  :custom
+  ;; Metals claims to support range formatting by default but it supports range
+  ;; formatting of multiline strings only. You might want to disable it so that
+  ;; emacs can use indentation provided by scala-mode.
+  (lsp-metals-server-args '("-J-Dmetals.allow-multiline-string-formatting=off"))
+  :hook (scala-mode . lsp))
+
 (use-package jdecomp
   :straight t
   :mode ("\\.class\\'" . jdecomp-mode)
