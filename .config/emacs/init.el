321cd759eb00aab90d5616dcafa4ec9b16c9fa4b
Andrey Listopadov <andreyorst@gmail.com>
2023-05-31 15:49:55 +0300

    configure proxy from system settings on start

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -459,6 +459,38 @@ disabled, or enabled and the mark is active."
 (use-package delsel
   :hook (after-init . delete-selection-mode))
 
+(use-package gsettings
+  :straight t)
+
+(use-package proxy
+  :after gsettings
+  :preface
+  (defun proxy--get-host (type)
+    (let ((host (gsettings-get (format "org.gnome.system.proxy.%s" type) "host")))
+      (and (not (string-empty-p host)) host)))
+  (defun proxy--get-port (type)
+    (let ((port (gsettings-get (format "org.gnome.system.proxy.%s" type) "port")))
+      (and (numberp port) port)))
+  (defun proxy--get-ignore ()
+    (let ((ignore (gsettings-get "org.gnome.system.proxy" "ignore-hosts")))
+      (and (vectorp ignore) (string-join ignore ","))))
+  (defun proxy-setup ()
+    (when-let ((host (proxy--get-host "http"))
+               (port (proxy--get-port "http"))
+               (ignore (proxy--get-ignore)))
+      (let ((proxy (format "http://%s:%s" host port)))
+        (setenv "http_proxy" proxy)
+        (setenv "https_proxy" proxy)
+        (setenv "ftp_proxy" proxy)
+        (setenv "no_proxy" ignore)
+        (setenv "HTTP_PROXY" proxy)
+        (setenv "HTTPS_PROXY" proxy)
+        (setenv "FTP_PROXY" proxy)
+        (setenv "NO_PROXY" ignore))))
+  (provide 'proxy)
+  :init
+  (proxy-setup))
+
 (use-package common-lisp-modes
   :straight (:host gitlab :repo "andreyorst/common-lisp-modes.el"))
 
