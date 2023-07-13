ce6eac5f6c8d09063191e199e06aa2b19943184c
Andrey Listopadov <andreyorst@gmail.com>
2023-07-13 18:54:44 +0300

    enchancements

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -99,8 +99,6 @@ refresh is greater than `package-automatic-refresh-threshold'."
   :init
   (proxy-setup-env))
 
-;; (use-package straight)
-
 (use-package delight
   :ensure t)
 
@@ -1577,19 +1575,19 @@ See `cider-find-and-clear-repl-output' for more info."
   (lsp-treemacs-theme "Iconless"))
 
 (use-package lsp-clojure
-  :no-require
-  :hook ((clojure-mode
-          clojurec-mode
-          clojurescript-mode)
-         . lsp))
-
-(use-package lsp-clojure
   :demand t
   :after lsp-mode
   :hook (cider-mode . cider-toggle-lsp-completion-maybe)
   :preface
   (defun cider-toggle-lsp-completion-maybe ()
     (lsp-completion-mode (if (bound-and-true-p cider-mode) -1 1))))
+
+(use-package lsp-clojure
+  :no-require
+  :hook ((clojure-mode
+          clojurec-mode
+          clojurescript-mode)
+         . lsp))
 
 (use-package lsp-java
   :ensure t
@@ -2248,6 +2246,10 @@ the generated command."
   :custom
   (message-kill-buffer-on-exit t))
 
+(use-package message-view-patch
+  :ensure t
+  :hook (gnus-part-display . message-view-patch-highlight))
+
 (use-package smtpmail
   :defer t)
 
@@ -2329,10 +2331,6 @@ the generated command."
     (let ((completion-styles (append completion-styles '(orderless))))
       (funcall fn prompt))))
 
-(use-package message-view-patch
-  :ensure t
-  :hook (gnus-part-display . message-view-patch-highlight))
-
 (use-package mu4e-alert
   :ensure t
   :after mu4e
@@ -2347,51 +2345,174 @@ the generated command."
 
 (use-package infinite
   :preface
-  (defvar-local infinite-base-frame nil)
-  (defvar-local infinite-frames nil)
+  (defvar infinite--base-frame nil)
+  (defvar infinite--frames nil)
 
-  (defun start-infinite ()
+  (defun infinite--track-mouse (event)
+    (interactive "e")
+    (track-mouse
+      (catch 'done
+        (while t
+          (let ((beginning-position (mouse-pixel-position))
+                new-event)
+            (setq new-event (read-event))
+            (if (not (eq (event-basic-type new-event) 'mouse-movement))
+                (when (eq (event-basic-type new-event) 'mouse-1)
+                  (throw 'done nil))
+              (let ((current-position (mouse-pixel-position)))
+                (setq x-pointer-shape 'hand)
+                (setq track-mouse 'dragging)
+                (dolist (f infinite--frames)
+                  (let* ((p (frame-position f))
+                         (x (car p))
+                         (y (cdr p)))
+                    (let ((new-x (- (cadr beginning-position)
+                                    (cadr current-position)))
+                          (new-y (- (cddr beginning-position)
+                                    (cddr current-position))))
+                      (modify-frame-parameters
+                       f
+                       `((user-position . t)
+                         (left . (+ ,(- x new-x)))
+                         (top . (+ ,(- y new-y)))))))))))))))
+
+
+
+  (defun infinite-start ()
     (interactive)
-    (let ((f (make-frame)))
-      (select-frame f)
-      (switch-to-buffer "*infinite*")
-      (infinite-mode)))
+    (delete-other-windows)
+    (switch-to-buffer " *infinite*")
+    (read-only-mode)
+    (infinite-mode))
 
   (define-derived-mode infinite-mode fundamental-mode
     "Infinite"
     "Create infinite space that can be panned with mouse and spawn windows on it."
-    (setq infinite-base-frame (window-frame (get-buffer-window))
-          infinite-frames nil))
+    (setq infinite--base-frame (window-frame (get-buffer-window))
+          infinite--frames nil
+          mode-line-format nil))
 
-  (defun infinite-delete-frame ()
+  (define-key infinite-mode-map [down-mouse-1] #'infinite--track-mouse)
+
+  (defun infinite-delete-frame (&optional f)
     (interactive)
-    (let ((frame (window-frame (get-buffer-window))))
-      (setq infinite-frames (delq frame infinite-frames))
-      (delete-frame frame)))
+    (let ((frame (or f (selected-frame))))
+      (when (memq frame infinite--frames)
+        (setq infinite--frames (remq frame infinite--frames))
+        (delete-frame frame))))
 
+  (defun infinite-open-buffer (buffer)
+    (interactive "B")
+    (infinite-make-frame (infinite-get-open-space) 20 buffer))
+
+  (defun infinite-visit-file (filename &rest _)
+    (interactive
+     (find-file-read-args "Find file: "
+                          (confirm-nonexistent-file-or-buffer)))
+    (let ((value (find-file-noselect filename)))
+      (message "%S" value)
+      (infinite-make-frame (infinite-get-open-space) 20 value)))
+
+  (defvar infinite-gap 10)
+
+  (defun infinite-get-open-space ()
+    (let ((xpos 0))
+      (dolist (f infinite--frames)
+        (setq xpos (max xpos (+ (car (frame-position f)) (frame-pixel-width f)))))
+      (+ xpos 5)))
+
+  (defun infinite-space-occupied-p (p1x p1y p2x p2y)
+    (catch 'yes
+      (dolist (f infinite--frames)
+        (let* ((pos (frame-position f))
+               (p3x (car pos))
+               (p3y (cdr pos))
+               (p4x (+ p3x (frame-pixel-width f)))
+               (p4y (+ p3y (frame-pixel-height f))))
+          (when (rectangle-overlap-p
+                 p1x p1y
+                 p2x p2y
+                 p3x p3y
+                 p4x p4y)
+            (throw 'yes f))))))
+
+  (defun rectangle-overlap-p ( p1x p1y p2x p2y
+                               p3x p3y p4x p4y)
+    ;;  p1__________
+    ;;   |          |
+    ;;   |     p3___|______
+    ;;   |      |   |      |
+    ;;   |      |   |      |
+    ;;   |______|___|      |
+    ;;          |   p2     |
+    ;;          |__________|
+    ;;                     p4
+    ;;
+    (not (or (< p2y p3y) (> p1y p4y) (< p2x p3x) (> p1x p4x))))
+
+(defun infinite-find-free-space (frame)
+  (catch 'pos
+    (let ((f frame)
+          (direction 'right))
+      (while t
+        (let* ((pos (frame-position f))
+               (x (car pos))
+               (y (cdr pos))
+               (pos (pcase direction
+                      ('right
+                       (cons (+ x (frame-pixel-width f) infinite-gap) y))
+                      ('left
+                       (cons (- x infinite--default-pixel-width infinite-gap) y))
+                      ('below
+                       (cons x (+ y (frame-pixel-height f) infinite-gap)))
+                      ('above
+                       (cons x (- y infinite--default-pixel-height infinite-gap))))))
+          (if-let ((new-f (infinite-space-occupied-p
+                           (car pos) (cdr pos)
+                           (+ (car pos) infinite--default-pixel-width)
+                           (+ (cdr pos) infinite--default-pixel-height))))
+              (setq f new-f
+                    direction (pcase direction
+                                ('right 'below)
+                                ('below 'left)
+                                ('left 'below)
+                                ('above 'right)))
+            (throw 'pos pos)))))))
+
+  (defun infinite-open-side-window (window &rest _)
+    (let* ((new-pos (infinite-find-free-space (window-frame window))))
+      (frame-selected-window
+       (infinite-make-frame (car new-pos) (cdr new-pos)))))
+  (defvar infinite--default-pixel-width 560)
+  (defvar infinite--default-pixel-height 380)
   (defun infinite-make-frame (x y &optional buffer)
-    (interactive)
     (let ((nframe (make-frame
                    `((left . ,x) (top . ,y)
-                     (width . 80) (height . 40)
-                     (parent-frame . ,infinite-base-frame)
+                     (width . 80) (height . 42)
+                     (parent-frame . ,infinite--base-frame)
                      (child-frame-border-width . 2)
                      (drag-with-header-line . t)
-                     (drag-internal-border . t)))))
-      (set-window-parameter
-       (frame-root-window nframe)
-       'header-line-format
-       '(:eval
-         (concat
-          " "
-          (propertize "[x]"
-                      'face '(:foreground "red")
-                      'mouse-face '(:foreground "red" :weight bold)
-                      'help-echo "Click to kill window frame"
-                      'action #'infinite-delete-frame)
-          " My Header Line Text ")))
-      (add-to-list 'infinite-frames nframe)
-      nil)))
+                     (drag-internal-border . t)
+                     (minibuffer . nil)))))
+      (setq infinite--default-pixel-width
+            (frame-pixel-width nframe)
+            infinite--default-pixel-height
+            (frame-pixel-height nframe))
+      (push nframe infinite--frames)
+      (select-frame nframe)
+      (when buffer
+        (switch-to-buffer buffer))
+      (let ((window (frame-root-window nframe)))
+        (set-window-parameter
+         window
+         'header-line-format
+         " %b")
+        (set-window-parameter
+         window
+         'split-window
+         #'infinite-open-side-window))
+      nil))
+  (provide 'infinite))
 
 
 (provide 'init)
