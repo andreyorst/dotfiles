76f92d973331e2a7e33c0335488f0b3239069dcb
Andrey Listopadov <andreyorst@gmail.com>
2023-09-29 18:05:40 +0300

    Handle Clojure warnings from project dependencies

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -88,6 +88,11 @@
            (dbus-color-theme-dark-p))
           (t nil)))
   (defun memoize (fn)
+    "Create a storage for FN's args.
+Checks if FN was called with set args before.  If so, return the
+value from the storage and don't call FN.  Otherwise calls FN,
+and saves its result in the storage.  FN must be referentially
+transparent."
     (let ((memo (make-hash-table :test 'equal)))
       (lambda (&rest args)
         (let ((value (gethash args memo)))
@@ -1616,8 +1621,8 @@ means save all with no questions."
       (if (and (null mode) project-compilation-mode)
           (append (list cmd project-compilation-mode) rest)
         args)))
-  (define-advice project-root (:around (fn project) expand-project-root)
-    (abbreviate-file-name (funcall fn project)))
+  (define-advice project-root (:filter-return (project) abbreviate-project-root)
+    (abbreviate-file-name project))
   (defun project-make-predicate-buffer-in-project-p ()
     (let ((project-buffers (project-buffers (project-current))))
       (lambda () (memq (current-buffer) project-buffers))))
@@ -1717,7 +1722,7 @@ means save all with no questions."
   :commands (define-compilation-mode)
   :preface
   (cl-defun compile-add-error-syntax
-      (mode name regexp &key file line col (level 'error))
+      (mode name regexp &key file line col (level 'error) hyperlink highlight)
     "Register new compilation error syntax.
 
 Add NAME symbol to `compilation-error-regexp-alist', and then add
@@ -1725,14 +1730,18 @@ REGEXP FILE LINE and optional COL LEVEL info to
 `compilation-error-regexp-alist-alist'."
     (or file (error "Missing value for :file keyword"))
     (or line (error "Missing value for :line keyword"))
-    (let ((level (cond ((eq level 'info) 0)
+    (let ((faces '(compilation-info-face
+                   compilation-warning-face
+                   compilation-error-face))
+          (level (cond ((eq level 'info) 0)
                        ((eq level 'warn) 1)
                        ((eq level 'error) 2)
                        (t (error "Mnsupported level type: %S" level))))
           (mode (symbol-name (or mode 'compilation))))
       (add-to-list (intern (concat mode "-error-regexp-alist")) name)
       (add-to-list (intern (concat mode "-error-regexp-alist-alist"))
-                   (list name regexp file line col level))))
+                   (list name regexp file line col level hyperlink
+                         (list highlight (nth level faces))))))
   (defmacro define-project-compilation-mode (base-name &rest body)
     (declare (indent 1))
     (let* ((name (symbol-name base-name))
@@ -1757,86 +1766,149 @@ Set automatically by the `" (symbol-name compilation-mode-name) "'."))
            ,(concat doc-name " Compilation")
            ,(concat "Compilation mode for " doc-name " output.")
            (setq-local ,current-project-root (project-current t))
-           (setq-local ,current-project-files (project-files ,current-project-root)))
-         ,@body
+           (setq-local ,current-project-files (project-files ,current-project-root))
+           ,@body)
          (provide ',compilation-mode-name)))))
 
 (use-package clojure-compilation-mode
   :no-require
   :preface
+  (defun clojure-compilation--split-classpath (classpath)
+    "Split the CLASSPATH string."
+    (split-string classpath ":" t "[[:space:]\n]+"))
+  (defun clojure-compilation--get-project-dependencies* (command _deps-file _mod-time)
+    "Call COMMAND to obtain the classpath string.
+DEPS-FILE and MOD-TIME are used for memoization."
+    (thread-last
+      command
+      shell-command-to-string
+      clojure-compilation--split-classpath
+      (seq-filter (lambda (s) (string-suffix-p ".jar" s)))))
+  (fset 'clojure-compilation--get-project-dependencies-memo
+        (memoize #'clojure-compilation--get-project-dependencies*))
+  (defun clojure-compilation--get-lein-project-dependencies (root)
+    "Obtain classpath from lein for ROOT."
+    (let* ((project-file (expand-file-name "project.clj" root))
+           (mod-time (file-attribute-modification-time (file-attributes project-file))))
+      (clojure-compilation--get-project-dependencies-memo
+       "lein classpath" project-file mod-time)))
+  (defun clojure-compilation--get-deps-project-dependencies (root)
+    "Obtain classpath from deps for ROOT."
+    (let* ((project-file (expand-file-name "deps.edn" root))
+           (mod-time (file-attribute-modification-time (file-attributes project-file))))
+      (clojure-compilation--get-project-dependencies-memo
+       "clojure -Spath" project-file mod-time)))
+  (defun clojure-compilation-get-project-dependencies (project)
+    "Get dependencies of the given PROJECT.
+Returns a list of all jar archives."
+    (when (bound-and-true-p tramp-gvfs-enabled)
+      (let ((root (project-root project)))
+        (cond ((file-exists-p (expand-file-name "deps.edn" root))
+               (clojure-compilation--get-deps-project-dependencies root))
+              ((file-exists-p (expand-file-name "project.clj" root))
+               (clojure-compilation--get-lein-project-dependencies root))))))
+  (defvar-local clojure-compilation-project-deps nil
+    "List of project's dependencies")
+  (defvar-local clojure-compilation-project-deps-mod-time nil
+    "Accumulated modification time of all project's libraries")
   (define-project-compilation-mode clojure-compilation
-    (defun clojure-compilation-filename-fn (rule-name)
-      "Create a function that gets filename from the error message.
-
-RULE-NAME is a symbol in `clojure-compilation-error-regexp-alist-alist'.
-It is used to obtain the regular expression, which used for a
-backward search in order to extract the filename from the first
-group."
-      (lambda ()
-        "Get a filname from the error message and compute relative directory."
-        (let* ((regexp (car (alist-get rule-name clojure-compilation-error-regexp-alist-alist)))
-               (filename (save-match-data
-                           (re-search-backward regexp)
-                           (substring-no-properties (match-string 1)))))
-          (if-let ((file (seq-find
-                          (lambda (s)
-                            (string-suffix-p filename s))
-                          clojure-compilation-current-project-files)))
-              (let* ((path-in-project
-                      (substring (expand-file-name file)
-                                 (length (expand-file-name (project-root clojure-compilation-current-project)))))
-                     (dir (substring path-in-project 0 (- (length filename)))))
-                (cons filename dir))
-            filename)))))
+    (require 'tramp-gvfs)
+    (setq-local clojure-compilation-project-deps
+                (clojure-compilation-get-project-dependencies
+                 clojure-compilation-current-project))
+    (setq-local clojure-compilation-project-deps-mod-time
+                (seq-reduce #'+ (mapcar (lambda (f)
+                                          (time-to-seconds
+                                           (file-attribute-modification-time
+                                            (file-attributes f))))
+                                        clojure-compilation-project-deps)
+                            0)))
+  (defun clojure-compilation--find-file-in-project (file)
+    "Check if FILE is part of the currently compiled project."
+    (seq-find
+     (lambda (s) (string-suffix-p file s))
+     clojure-compilation-current-project-files))
+  (defun clojure-compilation--file-exists-jar-p (jar file)
+    "Check if FILE is present in the JAR archive."
+    (with-temp-buffer
+      (when (zerop (call-process "jar" nil (current-buffer) nil "-tf" jar))
+        (goto-char (point-min))
+        (save-match-data
+          (re-search-forward (format "^%s$" (regexp-quote file)) nil t)))))
+  (defun clojure-compilation--find-dep* (file _project _deps-mod-time)
+    "Find FILE in current project dependency list.
+PROJECT and DEPS-MOD-TIME are used for memoizing the call."
+    (when (not (string-empty-p file))
+      (seq-find (lambda (d)
+                  (clojure-compilation--file-exists-jar-p d file))
+                clojure-compilation-project-deps)))
+  (fset 'clojure-compilation--find-dep-memo
+        (memoize #'clojure-compilation--find-dep*))
+  (defun clojure-compilation--find-dep (file)
+    "Find FILE in current project dependency list."
+    (clojure-compilation--find-dep-memo
+     file
+     clojure-compilation-current-project
+     clojure-compilation-project-deps-mod-time))
+  (defun clojure-compilation-filename ()
+    "Function that gets filename from the error message.
+If the filename comes from a dependency, try to guess the
+dependency artifact based on the project's dependencies."
+    (when-let ((filename (substring-no-properties (match-string 1))))
+      (or (clojure-compilation--find-file-in-project filename)
+          (when-let ((dep (clojure-compilation--find-dep filename)))
+            (concat (expand-file-name dep) "/" filename)))))
   :config
   (compile-add-error-syntax
-   'clojure-compilation
-   'clj-kondo-warning
+   'clojure-compilation 'some-warning
+   "^\\([^:[:space:]]+\\):\\([0-9]+\\) "
+   :file #'clojure-compilation-filename
+   :line 2 :level 'warn :hyperlink 1 :highlight 1)
+  (compile-add-error-syntax
+   'clojure-compilation 'clj-kondo-warning
    "^\\(/[^:]+\\):\\([[:digit:]]+\\):\\([[:digit:]]+\\): warning"
-   :file 1 :line 2 :col 3 :level 'warn)
+   :file 1 :line 2 :col 3 :level 'warn :hyperlink 1 :highlight 1)
   (compile-add-error-syntax
-   'clojure-compilation
-   'clj-kondo-error
+   'clojure-compilation 'clj-kondo-error
    "^\\(/[^:]+\\):\\([[:digit:]]+\\):\\([[:digit:]]+\\): error"
-   :file 1 :line 2 :col 3)
+   :file 1 :line 2 :col 3 :hyperlink 1 :highlight 1)
   (compile-add-error-syntax
-   'clojure-compilation
-   'kaocha-tap
-   "^not ok.*(\\([^:]*\\):\\([0-9]*\\))$"
-   :file (clojure-compilation-filename-fn 'kaocha-tap)
-   :line 2)
+   'clojure-compilation 'kaocha-tap
+   "^not ok.*(\\([^:]*\\):\\([0-9]*\\))"
+   :file #'clojure-compilation-filename
+   :line 2 :hyperlink 1 :highlight 1)
   (compile-add-error-syntax
-   'clojure-compilation
-   'clojure-fail
-   ".*\\(?:FAIL\\|ERROR\\) in.*(\\([^:]*\\):\\([0-9]*\\))$"
-   :file (clojure-compilation-filename-fn 'clojure-fail)
-   :line 2)
+   'clojure-compilation 'clojure-fail
+   "^.*\\(?:FAIL\\|ERROR\\) in.*(\\([^:]*\\):\\([0-9]*\\))"
+   :file #'clojure-compilation-filename
+   :line 2 :hyperlink 1 :highlight 1)
   (compile-add-error-syntax
-   'clojure-compilation
-   'clojure-reflection-warning
-   "^Reflection warning,[[:space:]]*\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\).*$"
-   :file (clojure-compilation-filename-fn 'clojure-reflection-warning)
+   'clojure-compilation 'clojure-reflection-warning
+   "^Reflection warning,[[:space:]]*\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\)"
+   :file #'clojure-compilation-filename
    :line 2 :col 3
-   :level 'warn)
+   :level 'warn :hyperlink 1 :highlight 1)
   (compile-add-error-syntax
-   'clojure-compilation
-   'clojure-performance-warning
-   "^Performance warning,[[:space:]]*\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\).*$"
-   :file (clojure-compilation-filename-fn 'clojure-performance-warning)
+   'clojure-compilation 'clojure-performance-warning
+   "^Performance warning,[[:space:]]*\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\)"
+   :file #'clojure-compilation-filename
    :line 2 :col 3
-   :level 'warn)
+   :level 'warn :hyperlink 1 :highlight 1)
   (compile-add-error-syntax
-   'clojure-compilation
-   'clojure-syntax-error
-   "^Syntax error .* at (\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\))\.$"
-   :file (clojure-compilation-filename-fn 'clojure-syntax-error)
+   'clojure-compilation 'clojure-syntax-error
+   "^Syntax error .* at (\\([^:]+\\):\\([0-9]+\\):\\([0-9]+\\))"
+   :file #'clojure-compilation-filename
    :line 2 :col 3)
   (compile-add-error-syntax
-   'clojure-compilation
-   'kaocha-unit-error
-   "^ERROR in unit (\\([^:]+\\):\\([0-9]+\\))$"
-   :file (clojure-compilation-filename-fn 'kaocha-unit-error)
-   :line 2))
+   'clojure-compilation 'kaocha-unit-error
+   "^ERROR in unit (\\([^:]+\\):\\([0-9]+\\))"
+   :file #'clojure-compilation-filename
+   :line 2 :hyperlink 1 :highlight 1)
+  (compile-add-error-syntax
+   'clojure-compilation 'eastwood-warning
+   "^\\([^:[:space:]]+\\):\\([0-9]+\\):\\([0-9]+\\):"
+   :file #'clojure-compilation-filename
+   :line 2 :col 3 :level 'warn :hyperlink 1 :highlight 1))
 
 (use-package fennel-compilation-mode
   :no-require
