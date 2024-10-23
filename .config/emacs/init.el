c867acce4fb71624b348838cd03afeb9b5474847
Andrey Listopadov <andreyorst@gmail.com>
2024-10-23 18:49:34 +0300

    remove lua-prettify

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1394,83 +1394,6 @@ name and a corresponding major mode."
    :remap 'lua-ts-mode
    :org-src '("lua" . lua-ts)))
 
-(use-package lua-prettify
-  :hook ((lua-mode lua-ts-mode) . lua-prettify-mode)
-  :delight lua-prettify-mode
-  :unless (in-termux-p)
-  :preface
-  (defgroup lua-prettify ()
-    "Lua prettification and ease of writing enchancements."
-    :prefix "lua-prettify-"
-    :group 'languages)
-  (defcustom lua-prettify-syntax-expansions
-    '(("def" "local function")
-      ("unless" "if not")
-      ("fn"  "function")
-      ("let" "local")
-      ("<-" "return"))
-    "List of abbreviarions and expansions for Lua"
-    :type '(repeat (list string string))
-    :group 'lua-prettify)
-  (defvar lua-prettify--original-syntax-table nil
-    "Original Lua syntax table.
-
-Syntax table is modified for abbreviation expansion to work on
-characters not considiered as word characters in original Lua table.
-This variable holds the original value to be restored once the mode is
-disabled.")
-  (defun lua-prettify--expand-abbrev-maybe ()
-    "Special advise for expanding abbreviations.
-
-Abbrevs that normally don't expand via abbrev-mode are handled manually."
-    (when (looking-back "<-" 1)
-      (delete-char -2)
-      (abbrev-insert (abbrev-symbol "<-"))))
-  (defun lua-prettify--cleanup ()
-    "Disable Lua prettification."
-    (setq prettify-symbols-alist nil)
-    (prettify-symbols-mode -1)
-    (abbrev-mode -1)
-    (remove-function
-     (local 'abbrev-expand-function)
-     #'lua-prettify--expand-abbrev-maybe)
-    (when lua-prettify--original-syntax-table
-      (set-syntax-table lua-prettify--original-syntax-table)
-      (setq lua-prettify--original-syntax-table nil)))
-  (defun lua-prettify--setup ()
-    "Setup Lua prettification."
-    (setq prettify-symbols-alist
-          (mapcar (lambda (abbrev-exp)
-                    (let ((abbrev (car abbrev-exp))
-                          (exp (cadr abbrev-exp)))
-                      `(,exp . ,(thread-last
-                                  abbrev
-                                  (mapcan
-                                   (lambda (ch)
-                                     (list '(Br . Bl) ch)))
-                                  cdr
-                                  vconcat))))
-                  lua-prettify-syntax-expansions))
-    (prettify-symbols-mode 1)
-    (let ((at (eval (intern (format "%s-abbrev-table" major-mode)))))
-      (dolist (abbrev-exp lua-prettify-syntax-expansions)
-        (apply #'define-abbrev at abbrev-exp)))
-    (setq lua-prettify--original-syntax-table (syntax-table))
-    (modify-syntax-entry ?- "w 12")
-    (abbrev-mode 1)
-    (add-function
-     :before (local 'abbrev-expand-function)
-     #'lua-prettify--expand-abbrev-maybe))
-  (define-minor-mode lua-prettify-mode
-    "Lua prettification and ease of writing enchancements."
-    :lighter " Lua Pretty"
-    :init-value nil
-    (if (and lua-prettify-mode
-             (not current-prefix-arg))
-        (lua-prettify--setup)
-      (lua-prettify--cleanup)))
-  (provide 'lua-prettify))
-
 (use-package elixir-ts-mode
   :defer t
   :when (treesit-p)
@@ -1585,6 +1508,11 @@ Abbrevs that normally don't expand via abbrev-mode are handled manually."
   :hook ((after-init . global-region-bindings-mode)
          ((elfeed-search-mode magit-mode mu4e-headers-mode)
           . region-bindings-off)))
+
+(use-package replace
+  :bind ( :map region-bindings-mode-map
+          ("k" . keep-lines)
+          ("f" . flush-lines)))
 
 (use-package puni
   :ensure t
@@ -2078,7 +2006,9 @@ dependency artifact based on the project's dependencies."
       (funcall fn require-match))))
 
 (use-package jdecomp
-  :ensure t
+  :vc ( :url "https://github.com/andreyorst/jdecomp"
+        :branch "master"
+        :rev :newest)
   :mode ("\\.class\\'" . jdecomp-mode)
   :preface
   (defvar cfr-path
